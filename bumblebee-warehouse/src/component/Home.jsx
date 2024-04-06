4f5eecd878f8ca70d1d52fec5a13712ade99f219
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-04-06 06:43:16 -0700

    feat(Home.jsx): import Sidebar and MainContent component

--- bumblebee-warehouse/src/component/Home.jsx
+++ bumblebee-warehouse/src/component/Home.jsx
@@ -1,9 +1,18 @@
-const Home = () =>{
-    return(
-        <>
-        </>
-    )
-}
+import { Grid } from "@mui/material";
+import Sidebar from "./Sidebar";
+import MainContent from "./MainContent";
 
+const Home = () => {
+  return (
+    <>
+      <Grid container>
+        <Sidebar />
+        <Grid sx={{ backgroundColor: "blue", height: "100vh" }} xs={8} >
+        <MainContent />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default Home
+export default Home;
