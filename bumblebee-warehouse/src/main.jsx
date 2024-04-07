6383466a2099c31b6c3868b45e217efd6a6d43f8
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-04-07 01:57:39 -0700

    feat(main.jsx): import and use ThemeProvider and theme

--- bumblebee-warehouse/src/main.jsx
+++ bumblebee-warehouse/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import Store from './page/store.js'
+import { ThemeProvider} from "@mui/material/styles";
+import { theme } from './theme/Theme.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App /> 
+      <ThemeProvider theme={theme}>
+         <App />
+      </ThemeProvider>
     </Provider>
    
   </React.StrictMode>,
