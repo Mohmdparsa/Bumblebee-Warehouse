733c33d179d7efa2b9f324f4f3526abbc7863e66
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-04-07 01:56:53 -0700

    feat(Theme.js): create Theme.js component for manage theme

---
+++ bumblebee-warehouse/src/theme/Theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main:"#e2eff1",
+            dark:"#b6d5e1"
+        },
+        secondary:{
+            main:"#65799b",
+            dark:"#555273"
+        }
+    }
+})
