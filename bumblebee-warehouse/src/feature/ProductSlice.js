8dedc078a5dd085bb4a40d839ae0c04ec72e028b
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-04-08 13:39:22 -0700

    feat(ProductSlice.js): add selectedItem selector for product data

--- bumblebee-warehouse/src/feature/ProductSlice.js
+++ bumblebee-warehouse/src/feature/ProductSlice.js
@@ -3,17 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const ProductSlice = createSlice({
   name: "product",
   initialState: {
-    data:[]
+    data: [],
   },
   reducers: {
     addStoreData: (state, action) => {
       state.data.push(action.payload);
     },
-    deleteStoreData : (state , action)=>{
-      state.data = state.data.filter((item , index)=>index !== action.payload)
-    }
+    deleteStoreData: (state, action) => {
+      state.data = state.data.filter((item, index) => index !== action.payload);
+    },
   },
 });
 
 export default ProductSlice.reducer;
 export const { addStoreData, deleteStoreData } = ProductSlice.actions;
+export const selectedItem = (state) => state.product.data;
