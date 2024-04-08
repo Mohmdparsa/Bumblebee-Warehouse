84be0fbb6a9705f35d87d05fe488ea51699103c4
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-04-08 07:00:11 -0700

    refactor(Home.jsx): update name and path of MainPage component

--- bumblebee-warehouse/src/component/Home.jsx
+++ bumblebee-warehouse/src/component/Home.jsx
@@ -1,14 +1,23 @@
 import { Grid } from "@mui/material";
 import Sidebar from "./Sidebar";
-import MainContent from "./MainContent";
+import MainPage from "./MainPage";
 
 const Home = () => {
   return (
     <>
       <Grid container>
         <Sidebar />
-        <Grid sx={{ backgroundColor: "secondary.main", height: "100vh" ,  overflowY: "auto",}} xs={7} md={8} lg={9}>
-        <MainContent/>
+        <Grid
+          sx={{
+            backgroundColor: "secondary.main",
+            height: "100vh",
+            overflowY: "auto",
+          }}
+          xs={7}
+          md={8}
+          lg={9}
+        >
+          <MainPage />
         </Grid>
       </Grid>
     </>
